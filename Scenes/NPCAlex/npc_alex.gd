3dfa3b3d670e1dcf68f2478db69e5e706e8089f7
Ladybugski <mi.aleixo@gmail.com>
2025-06-01 13:34:04 +0100

    5.9 Creating NPCs - Multiple Lines of Dialogue

--- Scenes/NPCAlex/npc_alex.gd
+++ Scenes/NPCAlex/npc_alex.gd
@@ -1,10 +1,21 @@
 extends StaticBody2D
 
+@export var dialog_lines: Array[String] = []
+var dialog_index: int = 0
+
 var can_interact: bool = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if !can_interact: $CanvasLayer.visible = false
 	if !can_interact or !Input.is_action_just_pressed('interact'): return
 	
-	$CanvasLayer.visible = !$CanvasLayer.visible
+	if dialog_index < dialog_lines.size():
+		$CanvasLayer.visible = true
+		get_tree().paused = true
+		
+		$CanvasLayer/DialogueText.text = dialog_lines[dialog_index]
+		dialog_index += 1
+	else:
+		$CanvasLayer.visible = false
+		get_tree().paused = false
+		dialog_index = 0
