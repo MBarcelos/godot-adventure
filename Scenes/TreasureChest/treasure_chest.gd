9c6f596ec3dec9a81e01547a68b3b8cc587a940a
Ladybugski <mi.aleixo@gmail.com>
2025-06-02 21:20:07 +0100

    6.7 Switch Puzzles - Remembering Opened Chests

--- Scenes/TreasureChest/treasure_chest.gd
+++ Scenes/TreasureChest/treasure_chest.gd
@@ -1,7 +1,13 @@
 extends StaticBody2D
 
+@export var chest_name: String
 var can_interact: bool = false
 var is_open: bool = false
+
+func _ready() -> void:
+	if SceneManager.opened_chest.has(chest_name):
+		is_open = true
+		$AnimatedSprite2D.play("open")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,6 +21,7 @@ func open_chest() -> void:
 	$AnimatedSprite2D.play("open")
 	$Sprite2D.visible = true
 	$Timer.start()
+	SceneManager.opened_chest.append(chest_name)
 
 func _on_timer_timeout() -> void:
 	$Sprite2D.visible = false
